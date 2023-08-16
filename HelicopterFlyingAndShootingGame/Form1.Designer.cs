1d4c0e7e04a7ef8aeb968195f638683715916162
Marcelo Andrés <marcelo.lam@alumnos.ucn.cl>
2023-08-15 23:09:23 -0400

    Se realizaron modificaciones adicionales a la interfaz.

--- HelicopterFlyingAndShootingGame/Form1.Designer.cs
+++ HelicopterFlyingAndShootingGame/Form1.Designer.cs
@@ -28,36 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.components = new System.ComponentModel.Container();
+            this.pilar1 = new System.Windows.Forms.PictureBox();
+            this.pilar2 = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.ufo = new System.Windows.Forms.PictureBox();
+            this.TemporizadorJuego = new System.Windows.Forms.Timer(this.components);
+            this.txtPje = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pilar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pilar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ufo)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // pilar1
             // 
-            this.pictureBox1.Image = global::HelicopterFlyingAndShootingGame.Properties.Resources.pillar;
-            this.pictureBox1.Location = new System.Drawing.Point(362, 207);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.pilar1.Image = global::HelicopterFlyingAndShootingGame.Properties.Resources.pillar;
+            this.pilar1.Location = new System.Drawing.Point(550, 0);
+            this.pilar1.Name = "pilar1";
+            this.pilar1.Size = new System.Drawing.Size(50, 150);
+            this.pilar1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pilar1.TabIndex = 0;
+            this.pilar1.TabStop = false;
+            this.pilar1.Tag = "pilar";
+            // 
+            // pilar2
+            // 
+            this.pilar2.Image = global::HelicopterFlyingAndShootingGame.Properties.Resources.pillar;
+            this.pilar2.Location = new System.Drawing.Point(250, 311);
+            this.pilar2.Name = "pilar2";
+            this.pilar2.Size = new System.Drawing.Size(50, 150);
+            this.pilar2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pilar2.TabIndex = 1;
+            this.pilar2.TabStop = false;
+            this.pilar2.Tag = "pilar";
+            // 
+            // player
+            // 
+            this.player.Image = global::HelicopterFlyingAndShootingGame.Properties.Resources.Halicopter;
+            this.player.Location = new System.Drawing.Point(100, 100);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(100, 54);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.player.TabIndex = 2;
+            this.player.TabStop = false;
+            // 
+            // ufo
+            // 
+            this.ufo.Image = global::HelicopterFlyingAndShootingGame.Properties.Resources.alien1;
+            this.ufo.Location = new System.Drawing.Point(700, 300);
+            this.ufo.Name = "ufo";
+            this.ufo.Size = new System.Drawing.Size(68, 72);
+            this.ufo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.ufo.TabIndex = 3;
+            this.ufo.TabStop = false;
+            // 
+            // TemporizadorJuego
+            // 
+            this.TemporizadorJuego.Enabled = true;
+            this.TemporizadorJuego.Interval = 20;
+            this.TemporizadorJuego.Tick += new System.EventHandler(this.MenuEventoTemporizador);
+            // 
+            // txtPje
+            // 
+            this.txtPje.AutoSize = true;
+            this.txtPje.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPje.ForeColor = System.Drawing.Color.White;
+            this.txtPje.Location = new System.Drawing.Point(10, 10);
+            this.txtPje.Name = "txtPje";
+            this.txtPje.Size = new System.Drawing.Size(158, 26);
+            this.txtPje.TabIndex = 4;
+            this.txtPje.Text = "Puntuación: 0";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.ClientSize = new System.Drawing.Size(919, 461);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(878, 461);
+            this.Controls.Add(this.txtPje);
+            this.Controls.Add(this.ufo);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.pilar2);
+            this.Controls.Add(this.pilar1);
             this.Name = "Form1";
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ShowIcon = false;
+            this.Text = "Juego Helicóptero";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pilar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pilar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ufo)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pilar1;
+        private System.Windows.Forms.PictureBox pilar2;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox ufo;
+        private System.Windows.Forms.Timer TemporizadorJuego;
+        private System.Windows.Forms.Label txtPje;
     }
 }
 
--- HelicopterFlyingAndShootingGame/Form1.cs
+++ HelicopterFlyingAndShootingGame/Form1.cs
@@ -12,9 +12,42 @@ namespace HelicopterFlyingAndShootingGame
 {
     public partial class Form1 : Form
     {
+        // Ingresaremos las variables dentro de la clase de la interfaz.
+
+        bool goUp; // Este es un booleano para permitir que el jugador pueda subir o ir hacia arriba.
+        bool goDown; // Este es un booleano para permitir que el jugador pueda bajar o ir hacia abajo.
+        bool disparar = false; // Esto verificará si el jugador ha disparado alguna bala o no.
+        int puntuacion = 0; // La puntuación total se inicializa en 0.
+        int velocidad = 8; // La velocidad inicial del juego es de 8 km/h.
+        Random rnd = new Random(); // Variable aleatoria.
+        int velocidadHelicoptero = 7; // La velocidad inicial del helicóptero es de 7 km/h.
+        int index; // Este es un entero vacío que se usará para cambiar las imágenes de ovnis.
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        // Método para el temporizador del juego.
+
+        private void MenuEventoTemporizador(object sender, EventArgs e)
+        {
+            // EN INSTANTES...
+        }
+
+        // Método que permite interactuar al jugador con la tecla presionada del teclado.
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            // EN INSTANTES...
+        }
+
+        // Método que permite interactuar al jugador soltando una tecla del teclado.
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            // EN INSTANTES...
         }
     }
 }
